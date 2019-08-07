72b4fec51a40a3b95ae5f64266940c82d3c6b027
Abby <ABBYZHANGNYC@GMAIL.COM>
2019-08-06 18:09:10 -0700

    fix error message

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -10,12 +10,13 @@ export class ConfigService {
   handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error("An error occurred:", error.error.message);
+      console.error("Client-side error occured: ", error.error.message);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+        `Server-side error occured, returned code ${error.status}, ` +
+          `body was: ${JSON.stringify(error.error)}`
       );
     }
     // return an observable with a user-facing error message
